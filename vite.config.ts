78f1cfed7b7700a9db30b1859f01891d2c2bfa5c
AndrewMysliuk <andre061120@gmail.com>
2024-11-21 10:32:32 +0200

    feat: ssl sertificate

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
+import fs from "fs"
 import eslintPlugin from "vite-plugin-eslint"
 
 // https://vitejs.dev/config/
@@ -22,8 +23,14 @@ export default defineConfig({
     },
   },
   server: {
+    https: {
+      // key: fs.readFileSync("/Users/andrewmysliuk/server.key"),
+      // cert: fs.readFileSync("/Users/andrewmysliuk/server.crt"),
+      key: fs.readFileSync("/etc/ssl/private/server.key"),
+      cert: fs.readFileSync("/etc/ssl/certs/server.crt"),
+    },
+    host: "localhost",
     port: 3000,
-    strictPort: true,
   },
   resolve: {
     alias: {
